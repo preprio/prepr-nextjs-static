f66092c45655d487ca807f51f3aa62235e6937ed
Kevin Quaedvlieg <k.quaedvlieg@outlook.com>
2025-04-11 16:36:16 +0200

    Fix payload

--- src/apollo-client.ts
+++ src/apollo-client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: `https://graphql.prepr.io/${process.env.PREPR_GRAPHQL_URL}`,
+  uri: `${process.env.PREPR_GRAPHQL_URL}`,
   cache: new InMemoryCache(),
 });
 
--- src/app/api/post-updated/route.ts
+++ src/app/api/post-updated/route.ts
@@ -3,11 +3,11 @@ import {revalidatePath, revalidateTag} from "next/cache";
 export async function POST(request: Request) {
   const body = await request.json();
 
-  if (!body.slug || !body?.slug['en-US']) {
+  if (!body.payload?.slug || !body?.payload?.slug['en-US']) {
     return new Response('Missing slug', { status: 400 });
   }
 
-  const slug = body.slug['en-US'];
+  const slug = body.payload.slug['en-US'];
 
   revalidatePath('/' + slug)
   revalidateTag('posts')
