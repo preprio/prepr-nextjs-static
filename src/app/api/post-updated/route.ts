d3e14bc2d5c5ae1cb0e913a4c34871575ae2f175
Kevin Quaedvlieg <k.quaedvlieg@outlook.com>
2025-06-17 12:33:22 +0200

    Added invalidation for all possible slugs

--- src/app/api/post-updated/route.ts
+++ src/app/api/post-updated/route.ts
@@ -3,13 +3,12 @@ import {revalidatePath, revalidateTag} from "next/cache";
 export async function POST(request: Request) {
   const body = await request.json();
 
-  if (!body.payload?.slug || !body?.payload?.slug['en-US']) {
+  if (!body.payload?.slug) {
     return new Response('Missing slug', { status: 400 });
   }
 
-  const slug = body.payload.slug['en-US'];
-
-  revalidatePath('/' + slug)
+  body.payload.slug.map((slug: string) => revalidatePath('/' + slug))
   revalidateTag('posts')
+
   return new Response('OK');
 }
